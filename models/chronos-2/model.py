fda42e6ff68de06a9145b8a0c2893bf6881f8705
Oleksandr Shchur <shchuro@amazon.com>
2026-05-06 15:15:01 +0000

    Add univariate mode for Chronos-2

--- models/chronos-2/model.py
+++ models/chronos-2/model.py
@@ -14,12 +14,14 @@ class Chronos2Model(fev.ForecastingModel):
         device: str = "cuda",
         batch_size: int = 100,
         cross_learning: bool = True,
+        as_univariate: bool = False,
     ):
         super().__init__()
         self.model_path = model_path
         self.device = device
         self.batch_size = batch_size
         self.cross_learning = cross_learning
+        self.as_univariate = as_univariate
 
     def _fit_predict(self, task: fev.Task) -> list[datasets.DatasetDict]:
         import torch
@@ -32,7 +34,7 @@ class Chronos2Model(fev.ForecastingModel):
         pipeline = BaseChronosPipeline.from_pretrained(model_path, device_map=self.device, torch_dtype=torch.float32)
 
         predictions_per_window, self.inference_time = pipeline.predict_fev(
-            task, batch_size=self.batch_size, cross_learning=self.cross_learning
+            task, batch_size=self.batch_size, cross_learning=self.cross_learning, as_univariate=self.as_univariate
         )
 
         return predictions_per_window
